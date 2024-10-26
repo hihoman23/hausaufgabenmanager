cd9a4bd9ec94c07eed94a966a17eadaab38a2147
Daniel Eichinger <oakdan@OakBook.local>
2024-10-26 10:19:05 +0200

    Add notifier

---
+++ helper/notifier.js
@@ -0,0 +1,26 @@
+export function getPermission(window) {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("Dieser Browser supported keine Notifications");
+    }  else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      const previousPermission = Notification.permission;
+      console.log(previousPermission)
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted" && previousPermission === 'default') {
+          const notification = new Notification("Jetzt informiert bleiben!");
+        }
+      });
+    }
+}
+  
+  
+export function sendNotification(task) {
+    const notifTitle = "Hausaufgabe bis morgen!";
+    const notifBody = task.title;
+    const options = {
+      body: notifBody,
+    };
+    new Notification(notifTitle, options);
+  }
