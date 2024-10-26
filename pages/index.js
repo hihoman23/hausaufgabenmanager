b1e281cd345d9d94457aaea8551f7505c780ceab
hihoman23 <henrikcodes@outlook.com>
2024-10-26 09:05:01 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,113 @@
-import Image from "next/image";
 import localFont from "next/font/local";
+import { useState } from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+let subjects = [
+  {
+    title: "Mathe",
+    tasks: [
+      {
+        title: "S. 33/14",
+        date: new Date(2024, 9, 26),
+        checked: false,
+        alarmed: false
+      }
+    ]
+  },
+  {
+    title: "Bio",
+    tasks: [
+      {
+        title: "S. 33 lesen",
+        date: new Date(2024, 9, 27), // Month: Oct == 9
+        checked: false,
+        alarmed: false
+      }
+    ]
+  }
+]
+
+const subjectNames = subjects.map(subject => subject.title)
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [taskTitle, setTaskTitle] = useState("")
+  const [selectedSubject, setSelectedSubject] = useState(subjectNames[0])
+  const [selectedDate, setSelectedDate] = useState(new Date())
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function showDialog(){
+    const dialog = document.getElementById("addDialog");
+    dialog.show()
+  }
+  function hideDialog(){
+    const dialog = document.getElementById("addDialog");
+
+    subjects.filter(subject => subject.title === selectedSubject)[0].tasks.push({title: taskTitle, date: selectedDate, checked: false})
+
+    setTaskTitle("")
+    setSelectedSubject(subjectNames[0])
+    setSelectedDate(Date.now())
+    dialog.close()
+  }
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <dialog id="addDialog">
+        <select value={selectedSubject} onChange={(event) => setSelectedSubject(event.target.value)}>
+          {subjectNames.map(subjectName => <option value={subjectName}>{subjectName}</option>)}
+        </select>
+        <input value={taskTitle} onChange={(event)=>setTaskTitle(event.target.value)}></input>
+        <input value={selectedDate} type="date" onChange={(event)=>setSelectedDate(event.target.value)}></input>
+        <button onClick={hideDialog}>Hinzuf&uuml;gen</button>
+      </dialog>
+      <button onClick={showDialog}>+</button>
+      <div className="main">
+        <div className="search">Suchbar</div>
+        <div className="subjects">
+          {subjects.map(subject => <Subject title={subject.title} tasks={subject.tasks}></Subject>)}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
+
+function isExpired(taskDate) {
+  const today = new Date()
+  const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
+  return (taskDate <= tomorrow)
+} 
+
+function checkForDeadline() {
+  for (let i = 0; i < subjects.length; i++) {
+    const tasks = subjects[i].tasks
+    for (let j = 0; j < tasks.length; j++) {
+      if (isExpired(tasks[j].date) && !tasks[j].checked && !tasks[j].alarmed) {
+        tasks[j].alarmed = true
+        alert("alarm")
+      }
+    }
+  }
+}
+
+setInterval(checkForDeadline, 10000)
+
+export function Subject(props) {
+
+  function saveSwitchState(newState, task) {
+    const isToggled = newState == "on"
+    console.log(isToggled)
+    //task.checked = isToggled
+  }
+
+  return (
+    <div className="subject">
+    <div className="subjectname"><span>{props.title}</span></div> 
+    <div className="tasks">
+        {props.tasks.map(task => 
+          <div className="task">
+            <input onChange={event => saveSwitchState(event.target.value, task)} type="checkbox"/>
+            <span className={isExpired(task.date) ? "expired" : ""}>{task.title}</span>
+          </div>
+        )}
+    </div>
+    </div>
+  )
+}
+
